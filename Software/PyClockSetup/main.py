ce564c090b7ba3a20da2e92b533adec828a9cad0
kuellenberg <andre@kuellenberg.eu>
2020-03-29 21:02:21 +0200

    Software

--- Software/PyClockSetup/main.py
+++ Software/PyClockSetup/main.py
@@ -12,10 +12,43 @@ class PyClockSetup(QtWidgets.QDialog):
         uic.loadUi('pyclocksetup.ui', self)
         self.show()
         self.cp.colorChanged.connect(self.updateColor)
+
+        self.serial = QSerialPort(self)
+        self.connectButton.clicked.connect(self.serialConnect)
+
+        availablePorts = QSerialPortInfo.availablePorts()
+        for port in availablePorts:
+            self.serialPorts.addItem(port.portName())
+            #if port.portName() == settings['serial_port']:
+            #    print(self.serial_ports.count())
+            #    self.serial_ports.setCurrentRow(self.serial_ports.count() - 1)
     
-    def updateColor(self, rgb):
-        print(rgb)
-        self.colorLabel.setStyleSheet('QLabel {{background-color: {}; }}'.format(rgb))
+    def serialConnect(self):
+        try:
+            self.serial.close()
+        except:
+            pass
+        port = self.serialPorts.currentText()
+        self.serial.setPortName(port)
+        if self.serial.open(QtCore.QIODevice.WriteOnly):
+            self.serial.setBaudRate(9600)
+            #self.serial.readyRead.connect(self.on_serial_read)
+        else:
+            QtWidgets.QMessageBox.warning(self, 'Warning', \
+                'Could not connect to {}.'.format(port))
+            return
+
+
+    def updateColor(self, hex):
+        (r,g,b,a) = QtGui.QColor(hex).getRgb()
+        #print(r,g,b,a)
+        rDiv = self.redDiv.value()
+        bDiv = self.blueDiv.value()
+        gDiv = self.greenDiv.value()
+        self.colorLabel.setStyleSheet('QLabel {{background-color: {}; }}'.format(hex))
+        str = 'color={},{},{}\n'.format(int(r/rDiv),int(g/gDiv),int(b/bDiv))
+        print(str)
+        self.serial.write(str.encode())
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
